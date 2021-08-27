a17254c5826fee33557574f5942cb3b0dfab15fb
Jayveersinh28 <69463767+Jayveersinh28@users.noreply.github.com>
2021-08-27 14:35:07 -0500

    Update ex4.c

--- week2/ex4.c
+++ week2/ex4.c
@@ -10,13 +10,13 @@ void swap(int *x, int *y)
 int main()
 {
 	int x, y;
-	printf("Enter a 1st number :");
+	printf("Enter the 1st number :");
 	scanf("\n%d", &x);
-	printf("\nEnter a 2nd number :");
+	printf("\nEnter the 2nd number :");
 	scanf("\n%d", &y);
-        printf("\nbefore swap | a -> %d, b-> %d", x, y);
+        printf("\nbefore swap a: %d, b: %d", x, y);
 	swap(&x, &y);
-	printf("after swap | a-> %d,b-> %d", x, y );
+	printf("after swap a: %d,b: %d", x, y );
 printf("\n");
 	return 0;
 }

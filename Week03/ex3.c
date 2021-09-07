938a66c0423de3c27fae4511baac1b02f43d40e7
Jayveersinh28 <69463767+Jayveersinh28@users.noreply.github.com>
2021-09-07 14:35:07 -0500

    Add files via upload

---
+++ Week03/ex3.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct node
+{
+	int value;
+	struct node *next;
+};
+
+void print_list(struct node *head)
+{
+	printf("{ ");
+	while(head != NULL)
+	{
+		printf("%d ", head->value);
+		head = head->next;
+	}
+	printf("}\n");
+}
+
+void insert_node(struct node *after, struct node *new_node)
+{
+	struct node *next = after->next;
+	after->next = new_node;
+	new_node->next = next;
+}
+
+void delete_node(struct node *head, struct node *to_delete)
+{
+	struct node *prev, *next;
+	// find previous node
+	while(head->next != to_delete) {
+        head = head->next;
+    }
+	prev = head;
+	next = to_delete->next;
+
+	prev->next = next;
+	to_delete->next = NULL;
+	free(to_delete);
+}
+
+int main() {
+    struct node *head = malloc(sizeof(struct node));
+    head->value = 0;
+    struct node *current, *prev = head;
+    srand(time(0));
+    int ran = (rand() % 1000);
+    for (int i = 1; i < ran; i++) {
+        current = malloc(sizeof(struct node));
+        current->value = i;
+        prev->next = current;
+        current->next = NULL;
+        prev = current;
+    }
+    print_list(head);
+
+    struct node *new_node = malloc(sizeof(struct node));
+    new_node->value = 20;
+    insert_node(head, new_node);
+    print_list(head);
+
+    delete_node(head, head->next->next->next);
+    print_list(head);
+
+
+    current = head;
+    while (current != NULL) {
+        struct node *temp = current;
+        current = current->next;
+        temp->next = NULL;
+        free(temp);
+    }
+    head = NULL;
+
+    print_list(head);
+
+    return 0;
+
+}

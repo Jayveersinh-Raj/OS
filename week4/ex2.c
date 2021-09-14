11f5e788252a535eecb258cd1d46a8c32595c915
Jayveersinh28 <69463767+Jayveersinh28@users.noreply.github.com>
2021-09-14 11:16:46 -0500

    Add files via upload

--- week4/ex2.c
+++ week4/ex2.c
@@ -1,25 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 int main(){
-pid_t pid = fork();
-   if (pid == -1) {
-      perror("fork failed");
-      exit(EXIT_FAILURE);
-   }
-   else if (pid == 0) {
-     fork();
+fork();
 fork();
 fork();
 sleep(5);
-      _exit(EXIT_SUCCESS);
-   }
-   else {
-      int status;
-      waitpid(pid, &status, 0);
-   }
    return EXIT_SUCCESS;
 }
